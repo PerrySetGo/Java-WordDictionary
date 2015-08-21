38ca297d16b6a7aa7b1ba119d4db8dfed021e5f6
Perry <lectique@gmail.com>
2015-08-21 10:52:35 -0700

    add dictionary class and test to confirm dictionary instantiates correctly.

--- src/main/java/Dictionary.java
+++ src/main/java/Dictionary.java
@@ -1,3 +1,11 @@
+import java.util.ArrayList;
+
 class Dictionary {
-  
-}
+private ArrayList<Word> mWords;
+
+public Dictionary(){
+mWords = new ArrayList<Word>();
+} //close constructor
+
+
+}//close main class
--- src/test/java/DictionaryTest.java
+++ src/test/java/DictionaryTest.java
@@ -2,10 +2,10 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class DictionaryTest {
-  //@Test
-  // public void dictionary_instatiatesCorrectly_true() {
-  //   Deck testDictionary = new Dictionary();
-  //   assertEquals(true, testDictionary instanceof Dictionary);
-  // }
+  @Test
+  public void dictionaryInstantiatesCorrectly_true() {
+    Dictionary testDictionary = new Dictionary();
+    assertEquals(true, testDictionary instanceof Dictionary);
+  }
 
 }
